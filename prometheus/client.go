71f36a358a10153871435b8a8c41b2d1eff9618c
iagoaraujo <joseiago.p.a@gmail.com>
2018-06-06 19:23:06 -0300

    Create prometheus client to collect requests metrics (#5)
    * Create prometheus client to collect requests metrics
    
    * Panic if client was not initialized
    
    * Create utils to use prometheus client as middlewares

---
+++ prometheus/client.go
@@ -0,0 +1,80 @@
+package prometheus
+
+import (
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	requestsMapStatus = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "io_http_requests_total",
+		Help: "The total number of requests which were performed.",
+	}, []string{"account", "workspace", "method", "path", "status"})
+
+	requestsMapCurrent = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "io_http_requests_current",
+		Help: "The current number of requests in course.",
+	}, []string{"account", "workspace", "method", "path"})
+
+	requestsMapDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name: "io_http_request_duration_seconds",
+		Help: "The duration of the requests in seconds.",
+	}, []string{"account", "workspace", "method", "path"})
+)
+
+var client PrometheusClient
+
+type PrometheusClient interface {
+	OpenRequest(req RequestData)
+	ObserveDuration(req RequestData, initTime time.Time)
+	CloseRequest(req RequestData, status string)
+}
+
+type prometheusClient struct {
+}
+
+type RequestData struct {
+	Account, Workspace, Method, Path string
+}
+
+func (p *prometheusClient) OpenRequest(req RequestData) {
+	labels := getDefaultLabels(req)
+	requestsMapCurrent.With(labels).Inc()
+}
+
+func (p *prometheusClient) ObserveDuration(req RequestData, initTime time.Time) {
+	labels := getDefaultLabels(req)
+	requestsMapDuration.With(labels).Observe(time.Since(initTime).Seconds())
+}
+
+func (p *prometheusClient) CloseRequest(req RequestData, status string) {
+	labels := getDefaultLabels(req)
+	requestsMapCurrent.With(labels).Dec()
+
+	labels["status"] = status
+	requestsMapStatus.With(labels).Inc()
+}
+
+func getDefaultLabels(req RequestData) prometheus.Labels {
+	return prometheus.Labels{"account": req.Account, "workspace": req.Workspace, "method": req.Method, "path": req.Path}
+}
+
+func InitClient() {
+	if client != nil {
+		panic("The client has already been initialized.")
+	}
+
+	prometheus.MustRegister(requestsMapStatus)
+	prometheus.MustRegister(requestsMapCurrent)
+	prometheus.MustRegister(requestsMapDuration)
+
+	client = &prometheusClient{}
+}
+
+func GetClient() PrometheusClient {
+	if client == nil {
+		panic("Init the prometheus client before access it")
+	}
+	return client
+}
---
+++ prometheus/utils.go
@@ -0,0 +1,31 @@
+package prometheus
+
+import (
+	"strconv"
+	"strings"
+	"time"
+
+	"gopkg.in/gin-gonic/gin.v1"
+)
+
+func InitRequest(g *gin.Context) {
+	GetClient().OpenRequest(getRequestInfo(g))
+}
+
+func Observe(g *gin.Context, init time.Time) {
+	GetClient().ObserveDuration(getRequestInfo(g), init)
+}
+
+func EndRequest(g *gin.Context) {
+	GetClient().CloseRequest(getRequestInfo(g), strconv.Itoa(g.Writer.Status()))
+}
+
+func getRequestInfo(g *gin.Context) RequestData {
+	path := strings.SplitN(g.Request.URL.Path, "/", 4)
+	return RequestData{
+		Account:   g.Param("account"),
+		Workspace: g.Param("workspace"),
+		Method:    g.Request.Method,
+		Path:      "/" + path[3],
+	}
+}
