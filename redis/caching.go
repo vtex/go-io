fdb3b8d2710e53ad16b4523824751cbff308a7da
Victor Elias <victorgelias@gmail.com>
2018-09-12 16:15:11 -0300

    Adopt changes from sphinx's Redis client
    Specifically:
     - Add a namespace before the actual key sent to Redis
     - Improve some error messages
     - Improve error log
     - Change interface of GetOrSet to get the function as last param
     - Additional nits from the actual version in sphinx

--- redis/caching.go
+++ redis/caching.go
@@ -17,10 +17,10 @@ const pingDelay = 60 * time.Second
 type Redis interface {
 	Get(key string, result interface{}) (bool, error)
 	Set(key string, value interface{}, expireIn time.Duration) error
-	GetOrSet(key string, result interface{}, fetch func() (interface{}, error), duration time.Duration) error
+	GetOrSet(key string, result interface{}, duration time.Duration, fetch func() (interface{}, error)) error
 }
 
-func New(endpoint string) Redis {
+func New(endpoint, keyNamespace string) Redis {
 	pool := &redis.Pool{
 		Dial: func() (redis.Conn, error) {
 			return redis.Dial("tcp", endpoint, redis.DialConnectTimeout(1*time.Second))
@@ -38,15 +38,21 @@ func New(endpoint string) Redis {
 		IdleTimeout: 3 * time.Minute,
 	}
 
-	return &redisC{pool: pool}
+	return &redisC{pool: pool, keyNamespace: keyNamespace}
 }
 
 type redisC struct {
-	lock sync.RWMutex
-	pool *redis.Pool
+	lock         sync.RWMutex
+	pool         *redis.Pool
+	keyNamespace string
 }
 
 func (r *redisC) Get(key string, result interface{}) (bool, error) {
+	key, err := remoteKey(r.keyNamespace, key)
+	if err != nil {
+		return false, err
+	}
+
 	reply, err := r.doCmd("GET", key)
 	if err != nil {
 		return false, errors.WithStack(err)
@@ -55,17 +61,22 @@ func (r *redisC) Get(key string, result interface{}) (bool, error) {
 	}
 
 	if bytes, ok := reply.([]byte); !ok {
-		return false, errors.Errorf("Unrecognized Redis reply: %s", reply)
+		return false, errors.Errorf("Unrecognized Redis response: %s", reply)
 	} else if err := json.Unmarshal(bytes, result); err != nil {
-		return false, errors.WithStack(err)
+		return false, errors.Wrap(err, "Failed to umarshal Redis response")
 	}
 	return true, nil
 }
 
 func (r *redisC) Set(key string, value interface{}, expireIn time.Duration) error {
+	key, err := remoteKey(r.keyNamespace, key)
+	if err != nil {
+		return err
+	}
+
 	bytes, err := json.Marshal(value)
 	if err != nil {
-		return errors.WithStack(err)
+		return errors.Wrap(err, "Failed to save data to Redis")
 	}
 
 	if _, err := r.doCmd("SET", key, bytes, "EX", int(expireIn.Seconds())); err != nil {
@@ -75,22 +86,11 @@ func (r *redisC) Set(key string, value interface{}, expireIn time.Duration) erro
 	return nil
 }
 
-func (r *redisC) doCmd(cmd string, args ...interface{}) (interface{}, error) {
-	conn := r.pool.Get()
-	reply, err := conn.Do(cmd, args...)
-	conn.Close()
-	return reply, err
-}
-
-func logError(err error, code, msg string) {
-	logrus.WithError(err).WithField("code", code).Error(msg)
-}
-
-func (c *redisC) GetOrSet(key string, result interface{}, fetch func() (interface{}, error), duration time.Duration) error {
-	if ok, err := c.Get(key, result); ok {
+func (r *redisC) GetOrSet(key string, result interface{}, duration time.Duration, fetch func() (interface{}, error)) error {
+	if ok, err := r.Get(key, result); ok {
 		return nil
 	} else if err != nil {
-		logError(err, "redis_cache_get_error", "Error getting data from redis")
+		logError(err, "redis_cache_get_error", r.keyNamespace, key, "Error getting data from redis")
 	}
 
 	value, err := fetch()
@@ -98,12 +98,30 @@ func (c *redisC) GetOrSet(key string, result interface{}, fetch func() (interfac
 		return err
 	}
 
-	c.Set(key, value, duration)
+	r.Set(key, value, duration)
+	return util.SetPointer(result, value)
+}
 
-	err = util.SetPointer(result, value)
-	if err != nil {
-		return err
+func (r *redisC) doCmd(cmd string, args ...interface{}) (interface{}, error) {
+	conn := r.pool.Get()
+	reply, err := conn.Do(cmd, args...)
+	conn.Close()
+	return reply, err
+}
+
+func logError(err error, code, namespace, key, msg string) {
+	logrus.WithError(err).
+		WithFields(logrus.Fields{
+			"code":         code,
+			"category":     "redis_cache",
+			"keyNamespace": namespace,
+			"key":          key,
+		}).Error(msg)
+}
+
+func remoteKey(ns, key string) (string, error) {
+	if key == "" {
+		return "", errors.Errorf("Cache key must not be empty")
 	}
-
-	return nil
+	return ns + ":" + key, nil
 }
