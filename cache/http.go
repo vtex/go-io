0b87a90a471a2b86bb7aebd8bbebf08e58dab55f
Victor Elias <victorgelias@gmail.com>
2019-05-24 15:42:33 -0300

    Remove enforced HTTP cache config

--- cache/http.go
+++ cache/http.go
@@ -7,11 +7,6 @@ import (
 	"github.com/gregjones/httpcache"
 )
 
-const (
-	maxCacheSize = 512 * 1024 * 1024  // 512MB
-	maxCacheAge  = 3 * 24 * time.Hour // 3 days
-)
-
-func HTTP() httpcache.Cache {
-	return lrucache.New(maxCacheSize, (int64)(maxCacheAge/time.Second))
+func HTTP(maxCacheSizeMiB int, maxCacheAge time.Duration) httpcache.Cache {
+	return lrucache.New(maxCacheSize*1024*1024, (int64)(maxCacheAge/time.Second))
 }
