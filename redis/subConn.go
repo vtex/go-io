e579f0f1dbb9d2dfb10d983814a3ee2302058047
Victor Elias <victorgelias@gmail.com>
2020-07-16 16:30:57 -0300

    Remove mutex :sparkles:
    All operations will be performed on the main loop
    so we don't need to have a mutex anymore :sunglasses:

--- redis/subConn.go
+++ redis/subConn.go
@@ -1,7 +1,6 @@
 package redis
 
 import (
-	"sync"
 	"time"
 
 	"github.com/gomodule/redigo/redis"
@@ -11,7 +10,6 @@ import (
 const pongTimeout = 5 * time.Second
 
 type subConn struct {
-	mu         sync.Mutex
 	pool       *redis.Pool
 	outputChan chan redis.Message
 
@@ -103,11 +101,9 @@ func (c *subConn) mainLoop() {
 				continue
 			}
 
-			c.mu.Lock()
 			for _, pattern := range patterns {
 				c.subscribedPatterns[pattern] = true
 			}
-			c.mu.Unlock()
 
 		case patterns := <-c.unsubscribeChan:
 			if err := c.currConn.PUnsubscribe(patterns...); err != nil {
@@ -116,11 +112,9 @@ func (c *subConn) mainLoop() {
 				continue
 			}
 
-			c.mu.Lock()
 			for _, pattern := range patterns {
 				delete(c.subscribedPatterns, pattern)
 			}
-			c.mu.Unlock()
 
 		case msg := <-msgChan:
 			switch v := msg.(type) {
@@ -165,9 +159,6 @@ func (c *subConn) resetConn() error {
 		psc.Close()
 		return errors.Wrap(err, "Failed to subscribe to test channel")
 	}
-
-	c.mu.Lock()
-	defer c.mu.Unlock()
 
 	if len(c.subscribedPatterns) > 0 {
 		patterns := make([]interface{}, 0, len(c.subscribedPatterns))
